5bf06bd83d05b95335d103c73435b4967a8191c2
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-07-29 16:17:38 +0900

    :tada: 프로젝트 초기화
    의존성
    - spring-boot-starter-web
    - spring-boot-starter-data-jpa
    - lombok
    - h2

---
+++ src/test/java/com/cenibee/project/studygroup/StudyGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenibee.project.studygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
